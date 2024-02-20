41befbe26f9a33ebd76042ef9dee328efae3e753
insufficientFunding <julian.nielsen1@outlook.com>
2024-02-20 11:45:47 +0100

    school

--- init.lua
+++ init.lua
@@ -1,22 +1,5 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-   vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-   })
-end
-vim.opt.rtp:prepend(lazypath)
-
-local lazy = require 'lazy'
-
 vim.g.mapleader = ' '
 
-lazy.setup {
-   { import = 'malware.plugins' },
-   { import = 'malware.plugins.lsp' },
-}
+require('malware.lazy')
+require('malware.config')
+
